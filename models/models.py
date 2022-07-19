d2b93ed2c1cbb3bcfcd5edf0db69c35a9f9b47e9
szh13818253048 <41218772+szh13818253048@users.noreply.github.com>
2022-07-19 18:19:15 +0800

    Update models.py

--- models/models.py
+++ models/models.py
@@ -7,21 +7,7 @@ def build_model(modelname,layer_size = 128,
                 dropout_rate = 0.3):
     model = keras.models.Sequential()
     model.add(keras.layers.Flatten(input_shape=[4**1+4**2+4**3+4**4+4**5+4**6+4**7+4**8+184*247]))
-    if modelname == 'FCN':
-        model.add(keras.layers.Dense(layer_size/2,kernel_initializer='glorot_uniform'))
-        model.add(keras.layers.LeakyReLU(alpha=0.05))
-        model.add(keras.layers.Dense(layer_size,kernel_initializer='glorot_uniform'))
-        model.add(keras.layers.LeakyReLU(alpha=0.05))
-        model.add(keras.layers.Dense(layer_size*2,kernel_initializer='glorot_uniform'))
-        model.add(keras.layers.LeakyReLU(alpha=0.05))
-        model.add(keras.layers.Dense(layer_size*4,kernel_initializer='glorot_uniform'))
-        model.add(keras.layers.Dense(5,activation="softmax"))
-        # accuracy_all: 0.8191276510604242
-        loss = CategoricalCrossentropy(label_smoothing=0.1)
-        model.compile(loss=loss,
-                        optimizer = keras.optimizers.Adam(learning_rate,decay=1e-3 / 200),
-                        metrics=['categorical_accuracy'])
-    elif modelname == "DeepmRNALoc":
+    if modelname == "DeepmRNALoc":
         model.add(keras.layers.Reshape((4**1+4**2+4**3+4**4+4**5+4**6+4**7+4**8+184*247,1)))
         model.add(keras.layers.Conv1D(64, 3,strides=2,padding="same"))
         model.add(keras.layers.BatchNormalization())
